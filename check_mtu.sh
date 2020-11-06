635481523d25ace77ceb68d4916c1595ed1ef339
Косимовский Александр <a.kosimovsky@crpt.ru>
2020-11-06 15:25:19 +0300

    added interface state in output if the script

--- check_mtu.sh
+++ check_mtu.sh
@@ -1,22 +1,31 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 #       script for checking MTU on linux interfaces excluding loop
-#	to use it via ssh on remote host ---- ssh user@ipaddress 'bash -s' < check_mtu.sh ----
+#       to use it via ssh on remote host ---- ssh user@ipaddress 'bash -s' < check_mtu.sh ----
 
 GREEN='\033[0;32m'
 RED='\033[0;31m'
+BLUE='\033[0;34m'
 NC='\033[0m'
+IP=$(which ip)
+STATE=""
 
 declare -a interfaces
 
-interfaces=$( ip link | grep '^[0-9]' | awk -F': ' '{print $2}' | sed 's/@.*//')
+interfaces=$($IP link | grep '^[0-9]' | awk -F': ' '{print $2}' | sed 's/@.*//')
 
 for i in $interfaces; do
         echo -ne "${GREEN}$i\t${NC}"
         echo -ne "${RED}------------------------------------${NC}"
-        ip link show $i | grep -oP '(?<=mtu )[^ ]*'
+        echo -ne $($IP link show $i | grep -oP '(?<=mtu )[^ ]*')
+        echo -n " "
+        STATE=$($IP link show $i | grep -oP '(?<=state )[^ ]*')
+        if [ ${#STATE} -eq 7 ]; then
+                echo -e "${BLUE}UNKNOWN${NC}"
+        else
+        [ ${#STATE} -gt 2 ] && echo -e "${RED}DOWN${NC}" || echo -e "${GREEN}UP${NC}"
+        fi
 done
 
 exit 0
 
-~
