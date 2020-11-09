1917e80d6fa54ca6990a949ee78cc78e65981758
Косимовский Александр <a.kosimovsky@crpt.ru>
2020-11-09 13:38:39 +0300

    ./check_mtu ip  --- now checks ipaddress of the interface

--- check_mtu.sh
+++ check_mtu.sh
@@ -2,6 +2,7 @@
 
 #       script for checking MTU on linux interfaces excluding loop
 #       to use it via ssh on remote host ---- ssh user@ipaddress 'bash -s' < check_mtu.sh ----
+#       ./check_mtu.sh ip       ---- checks ipv4 address of the interface
 
 GREEN='\033[0;32m'
 RED='\033[0;31m'
@@ -30,6 +31,11 @@ for i in $interfaces; do
         else
         [ ${#STATE} -gt 2 ] && echo -e "${RED}DOWN${NC}" || echo -e "${GREEN}UP${NC}"
         fi
+	case $1 in
+                ip)     echo -ne "\t\t$i IPaddress is "
+                        echo -e "${GREEN}$($IP -o -f inet a show $i | grep -oP '(?<=inet )[^ ]([0-9]\.*)+')${NC}\\n"
+                        ;;
+        esac
 done
 
 exit 0
